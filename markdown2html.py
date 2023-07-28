39284e7ddb09154d3f742ffd91db6f24c93b8fc0
luciel53 <l.leroty@gmail.com>
2023-07-28 17:41:40 +0200

    fix mod

--- markdown2html.py
+++ markdown2html.py
@@ -5,10 +5,12 @@ Write a script markdown2html.py that takes an argument 2 strings:
     First argument is the name of the Markdown file
     Second argument is the output file name
 
-R   equirements:
+    R  equirements:
 
-    If the number of arguments is less than 2: print in STDERR Usage: ./markdown2html.py README.md README.html and exit 1
-    If the Markdown file doesn’t exist: print in STDER Missing <filename> and exit 1
+    If the number of arguments is less than 2: print in STDERR Usage:
+    ./markdown2html.py README.md README.html and exit 1
+    If the Markdown file doesn’t exist: print in STDER Missing <filename>
+    and exit 1
     Otherwise, print nothing and exit 0
 """
 
