61ea4a4a259a6ccd7b528e5860361e8185fa4c32
luciel53 <l.leroty@gmail.com>
2023-07-28 18:37:25 +0200

    test1

--- markdown2html.py
+++ markdown2html.py
@@ -1,38 +1,35 @@
 #!/usr/bin/python3
 """
-A program to convert a markdown file to an html file
+A program to convert a markdown file in an html file
 """
+from sys import argv as arg
 
-from sys import argv
-
-def convert_md_html(md_file, html_file):
-    """Convert a markdown file to a html file"""
-    try:
-        # Open the md file, read it and stock it in the variable md_content
-        with open(md_file, 'r') as md:
-            md_content = md.read()
-
-    # if md_file does not exist, message error
-    except FileNotFoundError:
-        print("Missing <filename>")
-        exit(1)
-
-    # Open the output file in html
-    with open(html_file, 'w') as html:
-        # You can implement the conversion logic from Markdown to HTML here.
-        # For this example, I'll just write a simple HTML content.
-        html.write("")
 
 if __name__ == "__main__":
-    if len(argv) < 3:
+    """mod"""
+    if len(arg) < 3:
         print("Usage: ./markdown2html.py README.md README.html")
         exit(1)
     else:
         # Give arguments to the variables
-        md_file = argv[1]
-        html_file = argv[2]
+        md_file = arg[1]
+        html_file = arg[2]
 
-    # Use the function to convert Markdown to HTML
-    convert_md_html(md_file, html_file)
-    exit(0)
 
+def convert_md_html(md_file, html_file):
+    """Convert a markdown file in a html file"""
+    try:
+        # Open the md file, read it and stock it in the variable md_content
+        with open(md_file, 'r') as md:
+            md_content = md.read()
+    # if md_file does not exist, message error
+    except FileNotFoundError:
+        print("Missing <filename>")
+        exit(1)
+    # Open the output file in html
+    with open(html_file, 'w') as html:
+        html.write("")
+
+
+convert_md_html(md_file, html_file)
+exit(0)
