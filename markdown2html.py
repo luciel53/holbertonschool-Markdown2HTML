03deeea02c1a8f8e11dead359fa8909c4715044b
luciel53 <l.leroty@gmail.com>
2023-07-28 20:40:51 +0200

    sys fixed

--- markdown2html.py
+++ markdown2html.py
@@ -1,38 +1,37 @@
 #!/usr/bin/python3
-"""A program to convert a markdown file to an html file"""
+"""A program to convert a markdown file in an html file"""
+
 
 import sys
-import os
+
 
 def convert_md_html(md_file, html_file):
-    """Convert a markdown file to an html file"""
+    """Convert a markdown file in a html file"""
     try:
-        # Open the md file, read its content, and store it in the variable md_content
+        # Open the md file, read it and stock it in the variable md_content
         with open(md_file, 'r') as md:
             md_content = md.read()
 
-        # Open the output file in html
-        with open(html_file, 'w') as html:
-            # Write the converted content to the html file
-            html.write(md_content)
-
-        print(f"Conversion successful. HTML file created: {html_file}")
-
+    # if md_file does not exist, message error
     except FileNotFoundError:
-        # If md_file does not exist, display an error message on STDERR
-        sys.stderr.write(f"Missing {md_file}\n")
+        sys.stderr.write(f"Missing {md_file}")
         exit(1)
+
+    # Open the output file in html
+    with open(html_file, 'w') as html:
+        # Write "" in document
+        html.write("")
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
-        # If the number of arguments is less than 3, display usage information on STDERR
-        sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
         exit(1)
     else:
-        # Assign arguments to the variables
+        # Give arguments to the variables
         md_file = sys.argv[1]
         html_file = sys.argv[2]
 
-    # Use the function to convert markdown to html
+    # Use the method
     convert_md_html(md_file, html_file)
     exit(0)
