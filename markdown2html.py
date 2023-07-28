17b46859dae2d995bc656574784413b71267905b
luciel53 <l.leroty@gmail.com>
2023-07-28 18:16:11 +0200

    module

--- markdown2html.py
+++ markdown2html.py
@@ -8,26 +8,22 @@ if __name__ == "__main__":
     if len(arg) < 3:
         print("Usage: ./markdown2html.py README.md README.html")
         exit(1)
-
     else:
-        # else, give arguments to the variables
+        # Give arguments to the variables
         md_file = arg[1]
         html_file = arg[2]
 
 
 def convert_md_html(md_file, html_file):
     """Convert a markdown file in a html file"""
-
     try:
         # Open the md file, read it and stock it in the variable md_content
         with open(md_file, 'r') as md:
             md_content = md.read()
-
     # if md_file does not exist, message error
     except FileNotFoundError:
         print("Missing <filename>")
         exit(1)
-
     # Open the output file in html
     with open(html_file, 'w') as html:
         html.write("")
