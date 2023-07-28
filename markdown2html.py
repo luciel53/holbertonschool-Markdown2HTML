2e37fd0af1b6ca551681706ffca21347d988c070
luciel53 <l.leroty@gmail.com>
2023-07-28 17:47:14 +0200

    fix

--- markdown2html.py
+++ markdown2html.py
@@ -9,16 +9,14 @@ def convert_md_html(md_file, html_file):
     """Convert a markdown file in a html file"""
 
     try:
-        # Open the md file, read it and stock it in the variable md_content
+
         with open(md_file, 'r') as md:
             md_content = md.read()
 
-    # if md_file does not exist, message error
     except FileNotFoundError:
         print("Missing <filename>")
         exit (1)
 
-    # Open the output file in html
     with open(html_file, 'w') as html:
         html.write("")
 
@@ -27,16 +25,13 @@ def convert_md_html(md_file, html_file):
 if __name__ == "__main__":
     """To not execute this script"""
 
-    # if number of arguments is < 3, print an error
     if len(arg) < 3:
         print("Usage: ./markdown2html.py README.md README.html")
         exit (1)
 
     else:
-        #else, give arguments to the variables
         md_file = arg[1]
         html_file = arg[2]
 
-#Use the method
 convert_md_html(md_file, html_file)
 exit(0)
